b3ca1a5bacb7eb72127852852db72acb60c89902
Afro-kay <lindagreg654@gmail.com>
2024-05-18 12:39:07 +0000

    DCIT202-TASK-2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -9,3 +9,20 @@ function processArray(arr) {
         }
     });
 }
+
+function formatArrayStrings(stringArr, numberArr) {
+    if (stringArr.length !== numberArr.length) {
+        throw new Error("Both arrays must have the same length");
+    }
+
+    return stringArr.map((str, index) => {
+        const num = numberArr[index];
+        if (num % 2 === 0) {
+            // Capitalize the entire string if the number is even
+            return str.toUpperCase();
+        } else {
+            // Convert the string to lowercase if the number is odd
+            return str.toLowerCase();
+        }
+    });
+}
